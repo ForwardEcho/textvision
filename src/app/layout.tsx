4bd90db9b920d403b812355677e90e30dd19cffc
Zahid Azmy <zahidazmy13@gmail.com>
2026-01-15 23:53:40 +0800

    Fix layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import SmoothScroll from "../../components/SmoothScroll";
-import PageTransition from "../../components/PageTransition";
+import SmoothScroll from '../../components/SmoothScroll'
+import PageTransition from '../../components/PageTransition'
+import type { ReactNode } from 'react'
 
 const inter = Inter({
     subsets: ['latin'],
@@ -9,14 +10,18 @@ const inter = Inter({
     variable: '--font-inter',
 })
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: ReactNode
+}) {
     return (
         <html lang="en" className={inter.variable}>
         <body className="font-sans">
-            <SmoothScroll/>
-            <PageTransition>
-                {children}
-            </PageTransition>
+        <SmoothScroll />
+        <PageTransition>
+            {children}
+        </PageTransition>
         </body>
         </html>
     )
