ec1ae7f5ba469d2c6474b0b0d1e75da14c108eb5
Zahid Azmy <zahidazmy13@gmail.com>
2026-01-17 16:25:21 +0800

    add database port

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,6 +7,7 @@ const adapter = new PrismaMariaDb({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
     connectionLimit: 5
 });
 
