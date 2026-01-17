4e1e32c40fad5b1fea9ac82db1b377f8a94f7679
Zahid Azmy <zahidazmy13@gmail.com>
2026-01-17 16:32:42 +0800

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,12 +2,14 @@ import "dotenv/config";
 import {PrismaMariaDb} from "@prisma/adapter-mariadb";
 import {PrismaClient} from "@/generated/prisma/client";
 
+const dbPort = Number(process.env.DATABASE_PORT);
+
 const adapter = new PrismaMariaDb({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT,
+    port: dbPort,
     connectionLimit: 5
 });
 
